6ec3178e58c5d0e9a106834c5a61d718a961ed30
croftn <nyatcroft@gmail.com>
2024-04-08 15:44:05 -0600

    about, contact, part of policy

--- INTEXII/Controllers/HomeController.cs
+++ INTEXII/Controllers/HomeController.cs
@@ -18,15 +18,20 @@ namespace INTEXII.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
