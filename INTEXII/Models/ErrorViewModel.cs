28a98ca479683faa4f2d99b01d638df5054c4f61
croftn <nyatcroft@gmail.com>
2024-04-08 12:22:42 -0600

    Add project files.

---
+++ INTEXII/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INTEXII.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
