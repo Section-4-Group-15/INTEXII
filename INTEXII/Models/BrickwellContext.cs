ed41ac59ed401927834b3562b879755d9911bdb6
RedLynx101 <NoahHicks101@gmail.com>
2024-04-09 13:36:35 -0600

    Added new models to use category. Should add FKs later, but can use sql to do anything already

--- INTEXII/Models/BrickwellContext.cs
+++ INTEXII/Models/BrickwellContext.cs
@@ -24,6 +24,8 @@ public partial class BrickwellContext : IdentityDbContext<IdentityUser>
     public virtual DbSet<Order> Orders { get; set; }
 
     public virtual DbSet<Product> Products { get; set; }
+    public virtual DbSet<Category> Categories { get; set; }
+    public virtual DbSet<ProductCategory> ProductCategories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
