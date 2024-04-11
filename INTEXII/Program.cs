441d9202d6bfd36f01c004ce219363b2c8e593ab
jherm <jherm@JENSENSLAPTOP>
2024-04-11 10:47:46 -0600

    Some changes to IndProducts.cshtml

--- INTEXII/Program.cs
+++ INTEXII/Program.cs
@@ -14,6 +14,7 @@ services.AddAuthentication().AddGoogle(googleOptions =>
     googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
 });
 
+
 var connection = String.Empty;
 if (builder.Environment.IsDevelopment())
 {
