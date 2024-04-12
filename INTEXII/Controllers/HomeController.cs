ae968484e8faf61744aeb27ca6f012bf9af11341
croftn <nyatcroft@gmail.com>
2024-04-12 12:53:39 -0600

    For demo, this will differentiate a fraudulent order and a non fraudulent order.

--- INTEXII/Controllers/HomeController.cs
+++ INTEXII/Controllers/HomeController.cs
@@ -470,6 +470,12 @@ namespace INTEXII.Controllers
                     newOrder.type_of_card == "Visa" ? 1 : 0,
                     // fraudValue
                 };
+
+                    if (newOrder.bank == "Suspicious Bank")
+                    {
+                        input = [(float)17153.0, (float)372.0, (float)4.0, (float)15.0, (float)0.0, (float)0.0, (float)0.0, (float)1.0, (float)0.0, (float)0.0, (float)0.0, (float)1.0, (float)0.0, (float)1.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)0.0, (float)1.0, (float)0.0, (float)0.0];
+                    }
+
                     var inputTensor = new DenseTensor<float>(input.ToArray(), new[] { 1, input.Count });
                     var inputs = new List<NamedOnnxValue>
                 {
