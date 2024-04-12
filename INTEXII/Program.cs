1285f3e6b75733f9895b12213fc34c6480e08cdf
RedLynx101 <NoahHicks101@gmail.com>
2024-04-12 01:45:00 -0600

    Fix for local builds, so y'all can run it still

--- INTEXII/Program.cs
+++ INTEXII/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
 var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+//builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 var services = builder.Services;
 var configuration = builder.Configuration;
 
@@ -89,35 +89,35 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 });
 
 // Add Azure App Configuration to the container.
-var azAppConfigConnection = builder.Configuration["AppConfig"];
-if (!string.IsNullOrEmpty(azAppConfigConnection))
-{
-    // Use the connection string if it is available.
-    builder.Configuration.AddAzureAppConfiguration(options =>
-    {
-        options.Connect(azAppConfigConnection)
-        .ConfigureRefresh(refresh =>
-        {
-            // All configuration values will be refreshed if the sentinel key changes.
-            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
-        });
-    });
-}
-else if (Uri.TryCreate(builder.Configuration["Endpoints:AppConfig"], UriKind.Absolute, out var endpoint))
-{
-    // Use Azure Active Directory authentication.
-    // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
-    // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
-    builder.Configuration.AddAzureAppConfiguration(options =>
-    {
-        options.Connect(endpoint, new DefaultAzureCredential())
-        .ConfigureRefresh(refresh =>
-        {
-            // All configuration values will be refreshed if the sentinel key changes.
-            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
-        });
-    });
-}
+//var azAppConfigConnection = builder.Configuration["AppConfig"];
+//if (!string.IsNullOrEmpty(azAppConfigConnection))
+//{
+//    // Use the connection string if it is available.
+//    builder.Configuration.AddAzureAppConfiguration(options =>
+//    {
+//        options.Connect(azAppConfigConnection)
+//        .ConfigureRefresh(refresh =>
+//        {
+//            // All configuration values will be refreshed if the sentinel key changes.
+//            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+//        });
+//    });
+//}
+//else if (Uri.TryCreate(builder.Configuration["Endpoints:AppConfig"], UriKind.Absolute, out var endpoint))
+//{
+//    // Use Azure Active Directory authentication.
+//    // The identity of this app should be assigned 'App Configuration Data Reader' or 'App Configuration Data Owner' role in App Configuration.
+//    // For more information, please visit https://aka.ms/vs/azure-app-configuration/concept-enable-rbac
+//    builder.Configuration.AddAzureAppConfiguration(options =>
+//    {
+//        options.Connect(endpoint, new DefaultAzureCredential())
+//        .ConfigureRefresh(refresh =>
+//        {
+//            // All configuration values will be refreshed if the sentinel key changes.
+//            refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+//        });
+//    });
+//}
 builder.Services.AddAzureAppConfiguration();
 
 // Add services to the container.
@@ -166,7 +166,7 @@ app.UseStaticFiles();
 
 // Cookie Policy
 app.UseCookiePolicy();
-app.UseAzureAppConfiguration();
+//app.UseAzureAppConfiguration();
 
 app.UseRouting();
 
